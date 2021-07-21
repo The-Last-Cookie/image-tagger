43045a91cc5e49feae95b4495e3b8ce9cf5f4e35
The-Last-Cookie <koller.jonas@online.de>
2021-07-21 12:48:18 +0200

    finish implementation of PasswordManager class

--- passwordmanager.cpp
+++ passwordmanager.cpp
@@ -2,7 +2,10 @@
 
 PasswordManager::PasswordManager()
 {
-
+    m_minLength = 8;
+    m_minNumUpper = 3;
+    m_minNumUpper = 3;
+    m_specialChars = "!%$&/\\+#*";
 }
 
 PasswordManager::~PasswordManager()
@@ -12,31 +15,98 @@ PasswordManager::~PasswordManager()
 
 bool PasswordManager::checkPassword(QString password)
 {
-    return false;
+    if (password.length() < m_minLength) {
+        return false;
+    }
+
+    int numUpper = 0;
+    for (int i = 0; i < password.length(); i++) {
+        if (password.at(i).isUpper()) {
+            numUpper++;
+        }
+
+        if (numUpper >= m_minNumUpper) {
+            break;
+        }
+    }
+    if (numUpper < m_minNumUpper) {
+        return false;
+    }
+
+    int numLower = 0;
+    for (int i = 0; i < password.length(); i++) {
+        if (password.at(i).isUpper()) {
+            numLower++;
+        }
+
+        if (numLower >= m_minNumLower) {
+            break;
+        }
+    }
+    if (numLower < m_minNumLower) {
+        return false;
+    }
+
+    bool containsSpecialChar = false;
+    for (int i = 0; i < password.length(); i++) {
+        if (m_specialChars.contains(password.at(i))) {
+            containsSpecialChar = true;
+            break;
+        }
+    }
+    if (!containsSpecialChar) {
+        return false;
+    }
+
+    return true;
 }
 
 QString PasswordManager::hashPassword(QString password)
 {
-    return "";
+    QByteArray bytes = password.toLocal8Bit();
+    if (bytes.length() == 0) {
+        return "";
+    }
+
+    // Hash with SHA256
+    QByteArray hash = QCryptographicHash::hash(bytes, QCryptographicHash::Algorithm(4));
+    QString hashString = QString(hash);
+    return hashString;
 }
 
 bool PasswordManager::comparePasswordWithHash(QString password, QString hash)
 {
-    return false;
+    if (hash != hashPassword(password)) {
+        return false;
+    }
+
+    return true;
 }
 
 void PasswordManager::setMinLength(int minLength)
 {
+    if (minLength < 1) {
+        return;
+    }
+
     m_minLength = minLength;
 }
 
 void PasswordManager::setMinNumUpper(int minNumUpper)
 {
+    if (minNumUpper < 0) {
+        return;
+    }
+
     m_minNumUpper = minNumUpper;
 }
 
 void PasswordManager::setMinNumLower(int minNumLower)
 {
+    if (minNumLower < 0) {
+        return;
+    }
+
     m_minNumLower = minNumLower;
 }
 
