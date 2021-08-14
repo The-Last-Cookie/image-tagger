94284772c38a66c4d8abf42a103d969752df3bc1
The-Last-Cookie <koller.jonas@online.de>
2021-08-14 21:25:35 +0200

    add BasicAuthor

--- DataManagement/Author/basicauthor.cpp
+++ DataManagement/Author/basicauthor.cpp
@@ -4,3 +4,44 @@ BasicAuthor::BasicAuthor()
 {
 
 }
+
+BasicAuthor::~BasicAuthor()
+{
+
+}
+
+void BasicAuthor::setId(int id)
+{
+    if (m_id != id) {
+        m_id = id;
+    }
+}
+
+void BasicAuthor::setName(QString name)
+{
+    if (m_name != name) {
+        m_name = name;
+    }
+}
+
+void BasicAuthor::setDescription(QString description)
+{
+    if (m_description != description) {
+        m_description = description;
+    }
+}
+
+int BasicAuthor::getId()
+{
+    return m_id;
+}
+
+QString BasicAuthor::getName()
+{
+    return m_name;
+}
+
+QString BasicAuthor::getDescription()
+{
+    return m_description;
+}
--- DataManagement/Author/basicauthor.h
+++ DataManagement/Author/basicauthor.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <QString>
 
 class BasicAuthor
 {
-public:
-    BasicAuthor();
-};
+    public:
+        BasicAuthor();
+        ~BasicAuthor();
 
+        void setId(int id);
+        void setName(QString name);
+        void setDescription(QString description);
+
+        int getId();
+        QString getName();
+        QString getDescription();
+
+    private:
+        int m_id;
+        QString m_name;
+        QString m_description;
+};
